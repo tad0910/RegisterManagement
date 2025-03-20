f92f0d403e4abcb8c3ca95638144cfbeb7b109ed
tad0910 <165233573+tad0910@users.noreply.github.com>
2025-03-20 16:53:06 +0700

    Delete Services directory

--- Services/INotificationService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StudentInternshipManagement.Services
-{
-    public interface INotificationService
-    {
-        Task SendEmailAsync(string email, string subject, string message);
-    }
-}
